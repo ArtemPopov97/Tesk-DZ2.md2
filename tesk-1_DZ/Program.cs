45a84f37760b80900aa03e3685a4511ccb3aea93
unknown <artem.popov.97@mail.ru>
2022-06-16 18:13:38 +0300

    11

---
+++ tesk-1_DZ/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу,которая принимает на     Пример: 456->5
+// вход трёхзначное число и на выходе показывает                 782->8
+// вторую цифру этого числа.                                     918->1
+
+
+System.Console.Write("Введите число : ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int NumberB = NumberA / 10 % 10;
+
+int result = (NumberB);
+Console.WriteLine($"Результат: {result}");
